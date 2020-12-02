85813e51c0e36825ea5e70ce809e32102ffc1f09
ly-caohr <caohr@szlanyouc.om>
2020-12-02 12:33:24 +0800

    打成jar包

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class DemoApplicationTests {
+@WebAppConfiguration
+public class DemoApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void test() {
+
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaa");
+    }
+
+    @Test
+    public void test2() {
+
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaa");
     }
 
 }
